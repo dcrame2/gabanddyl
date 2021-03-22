6efab130019ca68e3f5d9551d12bd9fa6421b8cd
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-03-21 19:49:12 -0500

    more changes
    adding in transitions and more js

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ const container = document.querySelector('.container')
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+const gab = document.querySelector('.gab');
+const dyl = document.querySelector('.dyl');
+const lovers = document.querySelector('.lovers');
+
+const details = document.querySelector('.details');
+const details1 = document.querySelector('.details1');
+const about = document.querySelector('.about-header');
+
+gab.addEventListener('click', () => {
+    container.classList.add('hover');
+    lovers.style.visibility = 'hidden';
+    about.style.visibility = 'hidden';
+    details.classList.add('show');
+    details.style.display = 'contents';
+});
+
+dyl.addEventListener('click', () => {
+    container.classList.add('hover');
+    lovers.style.visibility = 'hidden';
+    about.style.visibility = 'hidden';
+    details1.classList.add('show');
+    details1.style.display = 'contents';
+});
