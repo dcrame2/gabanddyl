f1decac8cff755248b43077a2b3710b50d175c8d
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-03-27 20:31:17 -0500

    good stuff

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+// open.addEventListener('click', () => container.classList.add('show-nav'));
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+// close.addEventListener('click', () => container.classList.remove('show-nav'));
 
-const gab = document.querySelector('.gab');
-const dyl = document.querySelector('.dyl');
-const lovers = document.querySelector('.lovers');
+// const gab = document.querySelector('.gab');
+// const dyl = document.querySelector('.dyl');
+// const lovers = document.querySelector('.lovers');
 
-const details = document.querySelector('.details');
-const details1 = document.querySelector('.details1');
-const about = document.querySelector('.about-header');
+// const details = document.querySelector('.details');
+// const details1 = document.querySelector('.details1');
+// const about = document.querySelector('.about-header');
 
-gab.addEventListener('click', () => {
-    container.classList.add('hover');
-    lovers.style.visibility = 'hidden';
-    about.style.visibility = 'hidden';
-    details.classList.add('show');
-    details.style.display = 'contents';
-});
+// gab.addEventListener('click', () => {
+//     container.classList.add('hover');
+//     lovers.style.visibility = 'hidden';
+//     about.style.visibility = 'hidden';
+//     details.classList.add('show');
+//     details.style.display = 'contents';
+// });
 
-dyl.addEventListener('click', () => {
-    container.classList.add('hover');
-    lovers.style.visibility = 'hidden';
-    about.style.visibility = 'hidden';
-    details1.classList.add('show');
-    details1.style.display = 'contents';
+// dyl.addEventListener('click', () => {
+//     container.classList.add('hover');
+//     lovers.style.visibility = 'hidden';
+//     about.style.visibility = 'hidden';
+//     details1.classList.add('show');
+//     details1.style.display = 'contents';
+// });
+
+const toggle = document.querySelector('.toggle');
+const nav = document.querySelector('nav');
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+    nav.classList.toggle('active');
 });
