c6ad19543f6a4f593e241ff3e6dbb61bde15c076
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-05-11 20:28:39 -0500

    new updates may 11

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
 
 
 
-const toggle = document.querySelector('.toggle');
-const nav = document.querySelector('nav');
+// const toggle = document.querySelector('.toggle');
+// const nav = document.querySelector('nav');
 
 
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active');
-    nav.classList.toggle('active');
-});
+// toggle.addEventListener('click', () => {
+//     toggle.classList.toggle('active');
+//     nav.classList.toggle('active');
+// });
 
 
 
@@ -19,8 +19,19 @@ toggle.addEventListener('click', () => {
     $(".timeline.year2021").click(function(){
 
       $(".timeline-container").hide();
+      $('.timeline-header').hide();
       $('.wrapper').hide();
+      $('#loml').hide();
       $(".y2021").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2021").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+        $('.wrapper').show();
+        $('#loml').show();
+      })
 
       // toggle.classList.toggle('active');
       // toggle.addEventListener('click', () => {
@@ -32,17 +43,42 @@ toggle.addEventListener('click', () => {
 
     $(".timeline.year2020").click(function(){
 
-      $(".timeline-container").hide();
+      // $(".timeline-container").hide();
+      // $('.timeline-header').hide();
+      $('#about').hide();
       $('.wrapper').hide();
+      $('#loml').hide();
       $(".y2020").show();
+
+
+      $('.aboutus').click(function() {
+        
+        $(".y2020").hide();
+        // $(".timeline-container").show();
+        // $('.timeline-header').show();
+        $('#about').show();
+      $('.wrapper').show();
+      $('#loml').show();
+      });
     });
 
 
     $(".timeline.year2019").click(function(){
 
       $(".timeline-container").hide();
+      $('.timeline-header').hide();
       $('.wrapper').hide();
+      $('#loml').hide();
       $(".y2019").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2019").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+      $('.wrapper').show();
+      $('#loml').show();
+      });
     });
 
 
@@ -50,35 +86,90 @@ toggle.addEventListener('click', () => {
 
       $(".timeline-container").hide();
       $('.wrapper').hide();
+      $('.timeline-header').hide();
+      $('#loml').hide();
       $(".y2018").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2018").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+      $('.wrapper').show();
+      $('#loml').show();
+      });
     });
 
     $(".timeline.year2017").click(function(){
 
       $(".timeline-container").hide();
+      $('.timeline-header').hide();
       $('.wrapper').hide();
+      $('#loml').hide();
       $(".y2017").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2017").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+      $('.wrapper').show();
+      $('#loml').show();
+      });
     });
 
     $(".timeline.year2016").click(function(){
 
       $(".timeline-container").hide();
       $('.wrapper').hide();
+      $('.timeline-header').hide();
+      $('#loml').hide();
       $(".y2016").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2016").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+        $('.wrapper').show();
+        $('#loml').show();
+      });
     });
 
     $(".timeline.year2015").click(function(){
 
       $(".timeline-container").hide();
       $('.wrapper').hide();
+      $('#loml').hide();
+      $('.timeline-header').hide();
       $(".y2015").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2015").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+      $('.wrapper').show();
+      $('#loml').show();
+      });
     });
 
     $(".timeline.year2014").click(function(){
 
       $(".timeline-container").hide();
       $('.wrapper').hide();
+      $('.timeline-header').hide();
+      $('#loml').hide();
       $(".y2014").show();
+
+      $('.aboutus').click(function() {
+        
+        $(".y2014").hide();
+        $(".timeline-container").show();
+        $('.timeline-header').show();
+        $('.wrapper').show();
+        $('#loml').show();
+      });
     });
  
 
